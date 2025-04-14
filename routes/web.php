69c3bb3f8571c3156fc5eb2223130ec1ef4dcc48
Mohamed-Aljerjawi01 <mohamedahmedjerjawi@example.com>
2025-04-14 13:02:23 +0300

    TaskOne Push

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/TaskOne', function () {
+    // return ('TaskOne');
+    return view('TaskOne');
+});
